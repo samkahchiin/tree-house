ff2f11385ac52df6c42f50af8a93bdbb44e1d7c0
Sam Kah Chiin <samkahchiin@gmail.com>
2016-11-04 18:11:33 +0800

    [Finishes #133519651] Added template and controller actions

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,5 +4,11 @@ class BookingsController < ApplicationController
   end
 
   def create
+    Booking.create(booking_params)
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:first_name, :last_name, :identification_no, :phone_no, :email)
   end
 end
