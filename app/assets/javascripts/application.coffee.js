20ac213c2e301609385c6f25e8d2ad951826f889
samkahchiin <samkahchiin@gmail.com>
2016-11-05 21:40:18 +0800

    WIP - Added jquery-ui to the apps

--- app/assets/javascripts/application.coffee.js
+++ app/assets/javascripts/application.coffee.js
@@ -13,5 +13,9 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
+//= require jquery-ui
 //= require turbolinks
 //= require_tree .
+
+$ ->
+  $('.datepicker').datepicker()
