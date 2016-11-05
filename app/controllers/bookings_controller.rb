426162194df9c5733c72a25723fa4a7e583e0c57
samkahchiin <samkahchiin@gmail.com>
2016-11-05 14:35:19 +0800

    WIP - Refactor controller code.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -12,18 +12,25 @@ class BookingsController < ApplicationController
     # OR
     # Consider to utilize the booking model to store all the data
     description = 'Tree house booking'
-    callback_url = request.base_url + '/bookings/successful_callback'
+    callback_url = request.base_url
     extra_attributes = { description: description, collection_id: ENV['COLLECTION_ID'], amount: amount, callback_url: callback_url }
     bill_params = booking_params.merge extra_attributes
 
-    bill = Billplz::Bill.new bill_params
-    bill.create
+    @bill = Billplz::Bill.new bill_params
+    @bill.create
+    if @bill.success?
+      get_bill
+      # redirect to the bill page to make payment
+      redirect_to @successful_bil['url']
+    else
+      redirect_to root_url, alert: 'Something is wrong. Please fill in the form with correct information.'
+    end
+  end
 
-    bill_id = bill.parsed_json['id']
+  def get_bill
+    bill_id = @bill.parsed_json['id']
     bill = Billplz::Bill.new({ bill_id: bill_id})
-    bill = bill.get
-    # redirect to the bill page to make payment
-    redirect_to bill['url']
+    @successful_bil = bill.get
   end
 
   private
