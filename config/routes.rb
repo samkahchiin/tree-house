9c1be08d957e6b12e0af3bb26d6e23d670a8271e
Sam Kah Chiin <samkahchiin@gmail.com>
2017-01-02 23:44:55 +0800

    [Finishes #134977361] Create english page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :bookings, only: [:new, :create]
   post 'bookings/successful_callback'
 
+  get '/:locale' => "home#index"
   root to: "home#index"
 end
