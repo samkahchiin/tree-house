1e9dcb1e4a778d15dd4c2a5dc365f8bc5160acca
samkahchiin <samkahchiin@gmail.com>
2017-01-02 23:34:15 +0800

    WIP - Link is working for both page!

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,9 @@ module TreeHouse
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    I18n.enforce_available_locales = false
+    I18n.config.available_locales = :ch
+    config.i18n.default_locale = :ch
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
