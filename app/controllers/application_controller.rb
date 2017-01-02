299fb984f008708933434d89eb23d7bc0cca0bf7
samkahchiin <samkahchiin@gmail.com>
2017-01-02 23:43:34 +0800

    WIP - Minor refactor.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
+  private
   def default_url_options
     { locale: I18n.locale }
   end
