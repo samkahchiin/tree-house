ea3d0b1d290317a6e94fe3705a2ef9ca318f8aef
samkahchiin <samkahchiin@gmail.com>
2017-01-28 22:19:53 +0800

    WIP - Removed code since it is in gem oledi.

--- config/initializers/billplz.rb
+++ config/initializers/billplz.rb
@@ -8,11 +8,4 @@ end
 
 Billplz::Bill.module_eval do
   self.api_url = ENV['BILLPLZ_API'] + '/api/v3/bills'
-
-  def get
-    requires!(@payload, :bill_id)
-    @api_url = "#{@api_url}/#{@payload[:bill_id]}"
-    request(:get, nil)
-    JSON.parse(@response.body) if success?
-  end
 end
