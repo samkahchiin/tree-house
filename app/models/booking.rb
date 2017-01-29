dbe4e68b08d63109eaca80ea738ac0354d0cedc2
samkahchiin <samkahchiin@gmail.com>
2017-01-29 19:54:03 +0800

    WIP - Added validation for presence and spec. Passing specs.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,3 @@
 class Booking < ActiveRecord::Base
-  validates_presence_of :name, :mobile, :email
+  validates_presence_of :name, :mobile, :email, :no_of_adults, :trip_type, :price, :date, :backup_date_1, :backup_date_2
 end
